40c8aabf2a2f201db734dd22a504f83c6f2870d7
d0riven <wega315@gmail.com>
2018-03-12 00:53:54 +0900

    Feat: Add rule [!, process substitution]

--- bash2fish_translator.bash
+++ bash2fish_translator.bash
@@ -2,6 +2,8 @@
 
 set -e
 
+# not supported below:
+# -  Foo=foo echo $foo => env Foo=foo echo $foo
 function trans() {
   echo "$1" | grep 'alias' \
     | perl -pe 's/^(.+)?alias +(.+)$/alias \2/g' \
@@ -9,10 +11,13 @@ function trans() {
     | perl -pe 's/ +&& +/; and /g' \
     | perl -pe 's/ +\|\| +/; or /g' \
     | perl -pe 's/(\$)\?/\1status/g' \
+    | perl -pe 's/if +\[ +! +(.+) \] *; *then +(.+); +fi/if not test \1; \2; end/g' \
     | perl -pe 's/if +\[ (.+) \] *; *then +(.+); +fi/if test \1; \2; end/g' \
     | perl -pe 's/do *(.+); *done/\1; end/g' \
     | perl -pe 's/`(.*?)`/(\1)/g' \
-    | perl -pe 's/\$\(/(/g'
+    | perl -pe 's/\$\(/(/g' \
+    | perl -pe 's/<\((.*?)\)/(\1 \\\\| psub)/g' \
+    | perl -pe 's/ ! / not /g'
 }
 
 if [ -p /dev/stdin ]; then
